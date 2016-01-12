49392cdb68bd4f185fcd6284814d97767d014691
Geoff Yoerger <geoffreyiy1@gmail.com>
2016-01-12 12:00:53 -0600

    Smarter Template

--- src/Template/Main.java
+++ src/Template/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 	public static void main (String[] args) {
-
+		if(args.length < 1) {
+			die();
+		}
 	}	
+
+	public static void die() {
+		System.err.println("Usage: % arg1");
+		System.exit(1);
+	}
 }
