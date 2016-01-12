9acf1a6699595178c50c177fb032d26409abcc7b
Geoff Yoerger <geoffreyiy1@gmail.com>
2016-01-12 12:33:25 -0600

    2-15 (Snowman!)

--- src/2-15/Snowman.java
+++ src/2-15/Snowman.java
@@ -7,7 +7,8 @@ public class Snowman extends Applet {
 
 	public void paint (Graphics page) {
 
-		final int MID = 150;
+		final int MID = 170; // CHANGE: 150 -> 170
+		final int WIDTH = 300; // CHANGE: Add this
 		final int TOP = 50;
 
 		setBackground (Color.cyan);
@@ -16,7 +17,7 @@ public class Snowman extends Applet {
 		page.fillRect (0, 175, 300, 50); // ground
 
 		page.setColor (Color.yellow);
-		page.fillOval (-40, -40, 80, 80); // sun
+		page.fillOval (-40 + WIDTH, -40, 80, 80); // sun CHANGE: #1 -40 -> -40 + WIDTH
 
 		page.setColor (Color.white);
 
@@ -24,17 +25,23 @@ public class Snowman extends Applet {
 		page.fillOval (MID-35, TOP+35, 70, 50); // upper torso 
 		page.fillOval (MID-50, TOP+80, 100, 60); // lower torso
 
+		page.setColor(Color.red); // CHANGE: Add two red buttons
+		page.fillOval(MID-4, TOP+45, 8, 8);
+		page.fillOval(MID-5, TOP+60, 10, 10);
+
 		page.setColor (Color.black);
 
 		page.fillOval (MID-10, TOP+10, 5, 5); // left eye
 		page.fillOval (MID+5, TOP+10, 5, 5); // right eye
 
-		page.drawArc (MID-10, TOP+20, 20, 10, 190, 160); // smile
+		page.drawArc (MID-10, TOP+20, 20, 10, 10, 160); // smile CHANGE: #5 190 -> 10
 
 		page.drawLine (MID-25, TOP+60, MID-50, TOP+40); // left arm
 		page.drawLine (MID+25, TOP+60, MID+55, TOP+60); // right arm
 
 		page.drawLine (MID-20, TOP+5, MID+20, TOP+5); // brim of hat
 		page.fillRect (MID-15, TOP-20, 30, 25); // top of hat
+		
+		page.drawString("Geoff", 5, 20); // CHANGE: Me!
 	}
 }
